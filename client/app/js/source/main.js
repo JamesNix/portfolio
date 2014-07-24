d433cc1606de3ace1be7fd01ccab4acfabfdecfa
JamesNix <jamesfnix@gmail.com>
2014-07-24 17:00:43 -0500

    Working on theme changing button

--- client/app/js/source/main.js
+++ client/app/js/source/main.js
@@ -7,6 +7,27 @@
   $(document).ready(initialize);
 
   function initialize(){
+    $('#theme').click(changeTheme);
+  }
+
+  var theme = 1;
+
+  function changeTheme(){
+    $('body').css('background-color', bkgColor);
+  }
+
+  function bkgColor(){
+    var color;
+    switch(theme){
+      case 1:
+        color = '#fff';
+        theme += 1;
+        break;
+      case 2:
+        color = '#aaa';
+        theme = 1;
+    }
+
   }
 
 })();
