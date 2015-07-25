65f6e1163060abc8388c7dbfe71defd921b79741
JamesNix <jamesfnix@gmail.com>
2015-07-25 13:21:09 -0500

    Switched from connect to express module

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 var http = require('http');
-var connect = require('connect');
-var directory = 'public';
 var port = process.env.PORT || 3000;
 
-var app = connect()
-  .use(connect.logger(':remote-addr -> :method :url [:status]'))
-  .use(connect.static(directory));
-
-http.createServer(app).listen(port, function(){
-  console.log('Node server listening. Port: ' + port + ', Directory: ' + directory);
+var express = require('express');
+var app = express();
+app.use('/', express.static(__dirname + '/public'));
+app.listen(port, function() { console.log('Node server listening on port:' + port)
 });
 
